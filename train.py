d6c5ce2260fceeccbf9116c70010dc7367e1402f
lukas <lukeab@gmail.com>
2018-12-04 14:33:19 +0000

    setup for class

---
+++ train.py
@@ -0,0 +1,43 @@
+from preprocess import *
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.utils import to_categorical
+import wandb
+from wandb.keras import WandbCallback
+
+wandb.init()
+config = wandb.config
+
+config.max_len = 11
+config.buckets = 20
+
+
+# Save data to array file first
+save_data_to_array(max_len=config.max_len, n_mfcc=config.buckets)
+
+labels=["bed", "happy", "cat"]
+
+# # Loading train set and test set
+X_train, X_test, y_train, y_test = get_train_test()
+
+# # Feature dimension
+channels = 1
+config.epochs = 50
+config.batch_size = 100
+
+num_classes = 3
+
+X_train = X_train.reshape(X_train.shape[0], config.buckets, config.max_len, channels)
+X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len, channels)
+
+y_train_hot = to_categorical(y_train)
+y_test_hot = to_categorical(y_test)
+
+model = Sequential()
+model.add(Flatten(input_shape=(config.buckets, config.max_len, channels)))
+model.add(Dense(num_classes, activation='softmax'))
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])
+model.fit(X_train, y_train_hot, batch_size=config.batch_size, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
